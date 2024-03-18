c8d01ffd7604e37839a4e66ab98711e14df03c04
emmanuelU17 <emmanueluluabuike@gmail.com>
2024-03-17 20:13:49 -0400

    converted webserver to a Springboot server

--- integration/src/main/java/dev/integration/Main.java
+++
@@ -1,7 +0,0 @@
-package dev.integration;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- webserver/src/main/java/dev/webserver/Main.java
+++
@@ -1,7 +0,0 @@
-package dev.webserver;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
